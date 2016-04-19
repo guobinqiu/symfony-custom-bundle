ebac9ed31f2553833719fa517f58bdca44443b90
Guobin <guobin.qiu@d8aspring.com>
2016-04-19 14:50:48 +0800

    aa

--- DependencyInjection/DataspringDemoExtension.php
+++ DependencyInjection/DataspringDemoExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Dataspring\DemoBundle\DependencyInjection;
+namespace DataSpring\DemoBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
  */
-class DataspringDemoExtension extends Extension
+class DataSpringDemoExtension extends Extension
 {
     /**
      * {@inheritdoc}
