648a202cc4225926b337e5fb857aff236cfa5adc
Guobin <guobin.qiu@d8aspring.com>
2016-04-19 23:51:20 +0800

    ddd

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -2,12 +2,17 @@
 
 namespace Dataspring\DemoBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+
+    /**
+     * @Route("/demo/index", name="demo_index")
+     */
     public function indexAction()
     {
-        return $this->render('DataspringDemoBundle:Default:index.html.twig');
+        return $this->render('DataSpringDemoBundle:Default:index.html.twig');
     }
 }
